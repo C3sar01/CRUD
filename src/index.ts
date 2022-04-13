2bde376a8686fe280cc793f70dadbe641c9069da
C3sar01 <cesar.cssoto@gmail.com>
2022-04-13 09:42:43 -0400

    New feature/01

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,69 @@ import express from 'express';
 
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost/crudDB';
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+.then(() => console.log('Conectado a mongo'))
+.catch( (e: any) => console.log('El error de conexion es: '+ e));
+
+const itemsSchema = mongoose.Schema({
+    nombre: String,
+    precio: Number,
+    cantidad: Number,
+},{versionKey: false})
+
+const ItemsModel = mongoose.model('items',itemsSchema)
+
+
+//Mostrar items
+const mostrar = async () => {
+
+  const items = await ItemsModel.find()
+  console.log(items)
+}
+
+//crear items
+const crear = async () => {
+  const item = new ItemsModel({
+    nombre: 'NARANJA',
+    precio: 100,
+    cantidad: 10,         	
+  })
+
+  const resultado = await item.save()
+  console.log(resultado)
+}
+
+//Editar
+const actualizar = async (id:any) => {
+  const item = await ItemsModel.updateOne({_id:id},
+    {
+      $set: {
+        nombre: 'MANZANA ROJA',
+        precio: 170,
+        cantidad: 50,
+      }
+    })
+}
+
+//Borrar
+const eliminar = async (id:any) => {
+  const item = await ItemsModel.deleteOne({_id:id})
+  console.log(item)
+}
+
+
+
+//llamado a metodos
+// mostrar()
+// crear()
+actualizar('62546132521b235fa06f5142')
+eliminar('62546132521b235fa06f5142')
+
 /**
  * Required External Modules
  */
